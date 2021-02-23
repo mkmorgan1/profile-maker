ba2e441bfb992bd5fcc2dc0b227411f37ce9fe64
Matthew <m_kmorgan@hotmail.com>
2021-02-23 12:56:55 -0700

    created a couple routes I will use later

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
+/*  */
+
+/* Express Server and Port */
 import express from 'express';
 const app = express();
 const PORT = 8080;
+
+/* HOME */
+app.get('/', (req, res) => {
+
+});
+
+/* LOGIN */
+app.get('/login', (req, res ) => {
+
+});
+app.post('/login', (req, res ) => {
+
+});
+
+/* REGISTER */
+app.get('/register', (req, res ) => {
+
+});
+app.post('/register', (req, res ) => {
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
